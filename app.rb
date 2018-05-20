33e2737372fc2ae5acb645952f9d7bdfc8ec7ba5
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-20 11:25:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
+configure do 
+  enable :session 
+  set :session_secret "secret"
+end
+
+
 class App < Sinatra::Base
+  get '/' do 
+    erb :index 
+  end 
+  
+  post '/checkout' do 
+    session[:item] = @params[:item]
+    @session = session 
+    
+    erb :checkout 
+  end 
 end
